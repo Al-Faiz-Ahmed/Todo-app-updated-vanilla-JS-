5deff8041d3e480141d7bf9f10309f505a97af8d
Muhammad Faiz Ahmed <sheikhfaiz546@gmail.com>
2021-06-10 20:10:27 +0500

    some error fixed

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ function editItem(e) {
 
   for (var i = 0; i < lisarr.length; i++) {
     if (lisarr[i].nodeType === 1 && e.parentNode.id !== lisarr[i].id) {
-      lisarr[i].lastChild.remove();
+      lisarr[i].lastChild.style.display = 'none';
     }
   }
   e.parentNode.innerHTML = "Correcting List";
@@ -66,15 +66,21 @@ function editItem(e) {
 function saveList() {
   var deleteAll = document.getElementById("deleteAll");
   var AddItem = document.getElementById("addItem");
-  var value = document.getElementById("text").value;
+  var value = document.getElementById("text");
 
-  if (value !== "") {
+  if (value.value !== "") {
     var lisarr = list.childNodes;
+
     for (var i = 0; i < lisarr.length; i++) {
       // console.log(lisarr[i].childNodes[1]);
 
-      if (lisarr[i].nodeType === 1 && identify === lisarr[i].id) {
-        lisarr[i].innerHTML = value;
+      if (lisarr[i].nodeType === 1) {
+
+
+
+
+        if(identify === lisarr[i].id){
+        lisarr[i].innerHTML = value.value;
 
         // // delete buton
         var delbtn = document.createElement("button");
@@ -84,17 +90,24 @@ function saveList() {
         delbtn.setAttribute("onclick", "deleteItem(this)");
 
         //   // // edit btn
-        var editBtn = document.createElement("button");
-        var editText = document.createTextNode("Edit");
-        editBtn.appendChild(editText);
-        lisarr[i].appendChild(editBtn);
-        editBtn.setAttribute("onclick", "editItem(this)");
+        // var editBtn = document.createElement("button");
+        // var editText = document.createTextNode("Edit");
+        // editBtn.appendChild(editText);
+        // lisarr[i].appendChild(editBtn);
+        // editBtn.setAttribute("onclick", "editItem(this)");
 
         AddItem.innerHTML = "Add Item";
         AddItem.setAttribute("onclick", "addTodo(this)");
-
         deleteAll.style.display = "inline";
       }
+      
+      var editBtn = document.createElement("button");
+      var editText = document.createTextNode("Edit");
+      editBtn.appendChild(editText);
+      lisarr[i].appendChild(editBtn);
+      editBtn.setAttribute("onclick", "editItem(this)");
+    }
     }
   }
+  value.value = '';
 }
